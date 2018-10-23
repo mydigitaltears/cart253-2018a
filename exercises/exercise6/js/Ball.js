a8d4b466d0f79a3d72d1f873be9909425419f3d8
xavier <xavier.touikan@gmail.com>
2018-10-23 16:00:36 -0400

    Ex6: syntax error, added a "," between the two arguments

--- exercises/exercise6/js/Ball.js
+++ exercises/exercise6/js/Ball.js
@@ -55,7 +55,8 @@ Ball.prototype.isOffScreen = function () {
 //
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
-  rect(this.x this.y);
+  ////// FIXED ////// (added a , between the two arguments)
+  rect(this.x, this.y);
 }
 
 // handleCollision(paddle)
