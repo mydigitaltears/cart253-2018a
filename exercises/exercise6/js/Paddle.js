4e5a8d05eb6eaa1cf9fe996c10bea8bf4a9dd44c
xavier <xavier.touikan@gmail.com>
2018-10-23 17:01:40 -0400

    Ex6: removed the "-" so the paddle go up on up key

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -28,7 +28,8 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
 Paddle.prototype.handleInput = function() {
   ////// Fixed ////// (changed "keyDown" to "keyIsDown" and added "this." before the actual key)
   if (keyIsDown(this.upKey)) {
-    this.vy = -this.speed;
+    ////// Fixed ////// (removed the "-" so the paddle go up on upkey)
+    this.vy = this.speed;
   }
   else if (keyIsDown(this.downKey)) {
     this.vy = -this.speed;
