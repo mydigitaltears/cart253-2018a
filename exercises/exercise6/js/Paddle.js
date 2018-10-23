bc67436fd4629c93e1f2755892a335ee6abc5160
xavier <xavier.touikan@gmail.com>
2018-10-23 16:46:22 -0400

    Ex6: changed "hight" to "height"

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -40,7 +40,8 @@ Paddle.prototype.handleInput = function() {
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  this.y = constraint(this.y,0,hight-this.h);
+  ////// Fixed ////// (changed "hight" to "height")
+  this.y = constraint(this.y,0,height-this.h);
 }
 
 // display()
