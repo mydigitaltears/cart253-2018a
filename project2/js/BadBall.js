fb47877020295098857f9cc2790c3d9e64575804
Xavier <Xavier.touikan@gmail.com>
2018-11-05 21:10:44 -0500

    P2: started the BadBall object

---
+++ project2/js/BadBall.js
@@ -0,0 +1,28 @@
+// BadBall
+
+// BadBall constructor
+//
+// Sets the properties with the provided arguments
+function BadBall(x,y,vx,vy,size,speed) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.size = size;
+  this.speed = speed;
+}
+
+// update()
+BadBall.prototype.update = function () {
+  // Update position with velocity
+  this.x += this.vx;
+  this.y += this.vy;
+
+  // Constrain y position to be on screen
+  this.y = constrain(this.y,0,height-this.size);
+
+  // Check for touching upper or lower edge and reverse velocity if so
+  if (this.y === 0 || this.y + this.size === height) {
+    this.vy = -this.vy;
+  }
+}
