b00cac7125ed8900f16dbd11aabfd154ad77960a
Xavier <xavier.touikan@gmail.com>
2018-11-13 19:59:11 -0500

    E7: added libraries (p5 play and sound)

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,5 @@
+// Project 3 Prototype 1
+// I want to make a fun and simple roleplaying game
+// in which the player explores a natural kind of
+// environement and interacts with different elements
+// in nature. For this I'll use p5 play to use sprite animations
